dbb2fa32194aa8b2ba7de042829af36ba50b5f3b
Dilupa Sandika <foodforcelv@gmail.com>
2025-11-13 02:47:06 -0800

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
+// Tailwind integration එක import කරගන්නවා
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Tailwind integration එක Astro වලට add කරනවා
+  integrations: [tailwind()]
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // --- මේ අලුත් Line එක Add කරන්න ---
+  darkMode: 'class', 
+  // ---------------------------------
+
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
